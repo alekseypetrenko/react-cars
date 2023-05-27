c949f15875fa6a4749294a1aa22f995758077242
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-05-27 21:17:02 +0300

    Highlighted existing car

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -4,10 +4,15 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) =>
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCras = data.filter((car) =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase()),
     );
+
+    return {
+      cars: filteredCras,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -15,8 +20,11 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    // Decide if this cars should be bold
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
